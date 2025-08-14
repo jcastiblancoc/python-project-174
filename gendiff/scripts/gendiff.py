b69778b3d116086d26d540b032a80cea3661e8a4
jcastiblanco <castiblanco.jeisson@gmail.com>
2025-08-13 22:37:53 -0500

    Add gendiff script basic structure

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser(
@@ -8,6 +9,15 @@ def main():
     parser.add_argument("second_file", help="Second file to compare")
 
     args = parser.parse_args()
+
+    with open(args.first_file) as f1:
+        data1 = json.load(f1)
+
+    with open(args.second_file) as f2:
+        data2 = json.load(f2)
+
+    print("Contenido del primer archivo:", data1)
+    print("Contenido del segundo archivo:", data2)
 
 if __name__ == "__main__":
     main()
